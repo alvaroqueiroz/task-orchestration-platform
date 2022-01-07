c21da6bbc2ddc7f7b8a17d17fe758fead39299fb
leoalmeidasant <leonardo.santos@escale.com.br>
2022-01-07 20:00:09 -0300

    added test dag

--- dags-config/test_dag.py
+++ dags-config/test_dag.py
@@ -1,15 +1,52 @@
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
-    KubernetesPodOperator,
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime.utcnow(),
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+dag = DAG(
+    "kubernetes_sample",
+    default_args=default_args,
+    schedule_interval=timedelta(minutes=10),
 )
 
-k = KubernetesPodOperator(
-    name="hello-dry-run",
-    image="debian",
-    cmds=["bash", "-cx"],
-    arguments=["echo", "10"],
+
+start = DummyOperator(task_id="run_this_first", dag=dag)
+
+passing = KubernetesPodOperator(
+    namespace="airflow",
+    image="Python:3.6",
+    cmds=["Python", "-c"],
+    arguments=["print('hello world')"],
     labels={"foo": "bar"},
-    task_id="dry_run_demo",
-    do_xcom_push=True,
+    name="passing-test",
+    task_id="passing-task",
+    get_logs=True,
+    dag=dag,
 )
 
-k.dry_run()
+failing = KubernetesPodOperator(
+    namespace="airflow",
+    image="ubuntu:1604",
+    cmds=["Python", "-c"],
+    arguments=["print('hello world')"],
+    labels={"foo": "bar"},
+    name="fail",
+    task_id="failing-task",
+    get_logs=True,
+    dag=dag,
+)
+
+passing.set_upstream(start)
+failing.set_upstream(start)
